1de964effdf4b43a5538148a39a73dd062f5a503
SalamaSalama <salmohsid@alum.us.es>
2022-11-11 11:45:53 +0000

    First commit GET and  POST

---
+++ README.md
@@ -0,0 +1,3 @@
+contacts2_fis
+
+This example  repository contains the example code for the project thet we use in the course FIS 
---
+++ index.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var port = 3030;
+var BASE_API_PATH ="/api/v2";
+
+var contacts = [
+    {"name":"peter","phone":123456},
+    {"name":"john","phone":6668}
+];
+
+
+console.log("Starting API server ...");
+
+var app = express();
+app.use(bodyParser.json());
+
+app.get("/",(req,res) =>{
+
+    res.send("<html><body><h1>My server</h1></body></html>");
+});
+
+app.get(BASE_API_PATH + "/contacts",(req,res) =>{
+
+    console.log(Date() + " - GET /contacts");
+    res.send(contacts);
+});
+
+app.post(BASE_API_PATH +"/contacts", (req, res) =>{
+    console.log(Date() + " - POST /contacts");
+    var contact = req.body;
+    contacts.push(contact);
+    res.sendStatus(201);
+
+});
+
+app.listen(port);
+
+console.log("Server ready!");
